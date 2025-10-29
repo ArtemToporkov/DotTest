6a3a9a26dfd0d3ea873b88dba3e0aebf81ceac82
ArtemToporkov <toporkovartem259@gmail.com>
2025-10-29 15:21:52 +0500

    refactor: reorginize code
    add custom exception, move some methods, incapsulate some logic

--- run2.cs
+++ run2.cs
@@ -2,47 +2,72 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
+using System.Text;
 
 namespace DotTest;
 
 public interface IGraph
 {
-    public IReadOnlyCollection<string> Gateways { get; }
-    
-    public IEnumerable<string> GetSortedNeighbours(string node);
-    public bool TryRemoveEdge(string from, string to);
-    public PathsInfo FindShortestPathsInfo(string start);
-    public IGraph Clone();
-    public IEnumerable<(string u, string v)> GetEdges();
+    public IReadOnlySet<string> Gateways { get; }
+    public IReadOnlySet<(string FirstNode, string SecondNode)> Edges { get; }
+
+    public static abstract IGraph FromEdges(IReadOnlySet<(string, string)> edges);
+    public IReadOnlySet<string> GetNeighbours(string node, NeighboursSort sort = NeighboursSort.None);
+    public bool TryRemoveEdge(string firstNode, string secondNode);
+    public IReadOnlyDictionary<string, int> FindShortestPathsDistances(string startNode);
 }
 
-public record PathsInfo(IReadOnlyDictionary<string, int> Distances);
+public enum NeighboursSort
+{
+    None,
+    ByAscending,
+    ByDescending
+}
+
+public class GraphException(string message) : Exception(message);
 
 public class Graph : IGraph
 {
-    public IReadOnlyCollection<string> Gateways => _gateways;
-    
-    private readonly Dictionary<string, SortedSet<string>> _graph;
+    public IReadOnlySet<string> Gateways => _gateways;
+    public IReadOnlySet<(string FirstNode, string SecondNode)> Edges => _cachedEdges ??= GetEdges();
+
+    private const int MaxEdgesSupported = 100;
+    private const int MaxNodesSupported = 20;
+    private IReadOnlySet<(string FirstNode, string SecondNode)>? _cachedEdges;
+    private readonly Dictionary<string, SortedSet<string>> _adjacencyList;
     private readonly HashSet<string> _gateways;
 
-    public Graph(IEnumerable<(string, string)> edges)
+    private Graph(IReadOnlySet<(string, string)> edges)
     {
-        _graph = new();
+        _adjacencyList = new();
         _gateways = new();
 
+        if (edges.Count > MaxEdgesSupported)
+            throw new GraphException($"More than {MaxEdgesSupported} edges are not supported");
+        var nodes = new HashSet<string>();
         foreach (var (from, to) in edges)
         {
-            AddDirectedEdge(from, to);
-            AddDirectedEdge(to, from);
+            if (from == to)
+                throw new GraphException("Self-loops are not supported");
+            nodes.Add(from);
+            nodes.Add(to);
+            if (nodes.Count > MaxNodesSupported)
+                throw new GraphException($"More than {MaxNodesSupported} nodes are not supported");
+            AddDirectedEdgeOrThrow(from, to);
+            AddDirectedEdgeOrThrow(to, from);
             MarkIfGateway(from);
             MarkIfGateway(to);
         }
     }
 
-    private void AddDirectedEdge(string from, string to)
+    public static IGraph FromEdges(IReadOnlySet<(string, string)> edges) => new Graph(edges);
+
+    private void AddDirectedEdgeOrThrow(string from, string to)
     {
-        _graph.TryAdd(from, new(StringComparer.Ordinal));
-        _graph[from].Add(to);
+        _adjacencyList.TryAdd(from, new(StringComparer.Ordinal));
+        if (!_adjacencyList[from].Add(to))
+            throw new GraphException("Multiple edges are not supported");
+        _cachedEdges = null;
     }
 
     private void MarkIfGateway(string node)
@@ -51,61 +76,70 @@ public class Graph : IGraph
             _gateways.Add(node);
     }
     
-    public IEnumerable<string> GetSortedNeighbours(string node) => 
-        _graph.TryGetValue(node, out var neighbors) ? neighbors : Enumerable.Empty<string>();
-
-    public bool TryRemoveEdge(string from, string to)
+    public IReadOnlySet<string> GetNeighbours(string node, NeighboursSort sort = NeighboursSort.None)
     {
-        if (!_graph.TryGetValue(from, out var fromNeighbours) || !fromNeighbours.Contains(to))
-            return false;
-        if (!_graph.TryGetValue(to, out var toNeighbours) || !toNeighbours.Contains(from))
-            throw new InvalidOperationException(
-                "Domain invalid state: graph should contain both (from, to) and (to, from) edges");
+        if (!_adjacencyList.TryGetValue(node, out var neighbors))
+            return new HashSet<string>();
+        if (sort is NeighboursSort.None or NeighboursSort.ByAscending)
+            return neighbors;
+        
+        return neighbors.Reverse().ToHashSet();
+    }
 
-        fromNeighbours.Remove(to);
-        toNeighbours.Remove(from);
+    public bool TryRemoveEdge(string firstNode, string secondNode)
+    {
+        if (!_adjacencyList.TryGetValue(firstNode, out var firstNeighbours) 
+            || !firstNeighbours.Contains(secondNode))
+            return false;
+        if (!_adjacencyList.TryGetValue(secondNode, out var secondNeighbours) 
+            || !secondNeighbours.Contains(firstNode))
+            throw new GraphException(
+                $"Invalid domain state: graph should contain both ({nameof(firstNode)}, {nameof(secondNode)}) " +
+                $"and ({nameof(secondNode)}, {nameof(firstNode)}) edges");
+
+        firstNeighbours.Remove(secondNode);
+        secondNeighbours.Remove(firstNode);
+        _cachedEdges = null;
         return true;
     }
     
-    public PathsInfo FindShortestPathsInfo(string start)
+    public IReadOnlyDictionary<string, int> FindShortestPathsDistances(string startNode)
     {
-        var distances = new Dictionary<string, int> { [start] = 0 };
-        var cameFrom = new Dictionary<string, string>();
+        var distances = new Dictionary<string, int>
+        {
+            [startNode] = 0
+        };
         var queue = new Queue<string>();
 
-        if (_graph.ContainsKey(start))
-            queue.Enqueue(start);
+        if (_adjacencyList.ContainsKey(startNode))
+            queue.Enqueue(startNode);
 
         while (queue.Count > 0)
         {
             var currentNode = queue.Dequeue();
-            foreach (var neighbour in GetSortedNeighbours(currentNode))
+            foreach (var neighbour in GetNeighbours(currentNode, sort: NeighboursSort.ByAscending))
             {
                 if (!distances.ContainsKey(neighbour))
                 {
                     distances[neighbour] = distances[currentNode] + 1;
-                    cameFrom[neighbour] = currentNode;
                     queue.Enqueue(neighbour);
                 }
             }
         }
-        return new PathsInfo(distances);
+        return distances;
     }
     
-    public IEnumerable<(string u, string v)> GetEdges()
+    private IReadOnlySet<(string FirstNode, string SecondNode)> GetEdges()
     {
-        var edges = new List<(string u, string v)>();
-        foreach (var u in _graph.Keys.OrderBy(k => k, StringComparer.Ordinal))
-            foreach (var v in _graph[u])
-                if (StringComparer.Ordinal.Compare(u, v) < 0)
-                    edges.Add((u, v));
+        var edges = new HashSet<(string FirstNode, string SecondNode)>();
+        foreach (var u in _adjacencyList.Keys)
+            foreach (var v in _adjacencyList[u])
+                edges.Add((u, v));
+        
         return edges;
     }
 
-    public IGraph Clone() => new Graph(GetEdges());
-
-    private static bool IsGateway(string node) 
-        => !string.IsNullOrEmpty(node) && char.IsUpper(node[0]);
+    private static bool IsGateway(string node) => !string.IsNullOrEmpty(node) && char.IsUpper(node[0]);
 }
 
 public class VirusSolver<TGraph> where TGraph : IGraph
@@ -117,40 +151,24 @@ public class VirusSolver<TGraph> where TGraph : IGraph
 
     private bool TrySolve(TGraph graph, string virusPosition, out List<string> result)
     {
-        result = [];
+        result = new List<string>();
         
-        var stateKey = MakeStateKey(graph, virusPosition);
-        if (_seenStates.Contains(stateKey))
-            return false;
-        
-        var pathsFromVirus = graph.FindShortestPathsInfo(virusPosition);
-        if (!TryFindVirusBestTarget(graph, pathsFromVirus, out _))
-            return true;
-        
-        _seenStates.Add(stateKey);
+        if (TryHandleTerminalState(graph, virusPosition, out var isSolved))
+            return isSolved;
 
-        var candidateCuts = new List<(string gateway, string node)>();
-        foreach (var gateway in graph.Gateways.OrderBy(g => g, StringComparer.Ordinal))
-            foreach (var neighbour in graph.GetSortedNeighbours(gateway))
-                candidateCuts.Add((gateway, neighbour));
+        var candidateCuts = GenerateCandidateCuts(graph);
         
         foreach (var (gatewayToCut, nodeToCut) in candidateCuts)
         {
-            var graphClone = (TGraph)graph.Clone();
+            var graphClone = (TGraph)TGraph.FromEdges(graph.Edges);
             graphClone.TryRemoveEdge(gatewayToCut, nodeToCut);
 
-            var pathsAfterCut = graphClone.FindShortestPathsInfo(virusPosition);
-            if (!TryFindVirusBestTarget(graphClone, pathsAfterCut, out var newTargetGateway))
+            if (!TrySimulateVirusMove(graphClone, virusPosition, out var nextVirusPosition))
             {
                 result.Add($"{gatewayToCut}-{nodeToCut}");
                 return true; 
             }
             
-            var distancesToNewTarget = graphClone.FindShortestPathsInfo(newTargetGateway);
-            if (!TryFindNextStepTowardsTarget(
-                    graphClone, virusPosition, distancesToNewTarget, out var nextVirusPosition))
-                continue;
-
             if (char.IsUpper(nextVirusPosition[0]))
                 continue;
 
@@ -165,24 +183,65 @@ public class VirusSolver<TGraph> where TGraph : IGraph
         return false;
     }
 
-    private static string MakeStateKey(TGraph graph, string virusPosition)
+    private bool TryHandleTerminalState(TGraph graph, string virusPosition, out bool isSolved)
     {
-        var gatewayEdges = new List<string>();
+        isSolved = false;
+        var stateKey = CreateStateKey(graph, virusPosition);
+        if (_seenStates.Contains(stateKey))
+            return true; 
+
+        var distances = graph.FindShortestPathsDistances(virusPosition);
+        if (!TryFindVirusBestTarget(graph, distances, out _))
+        {
+            isSolved = true;
+            return true;
+        }
+        
+        _seenStates.Add(stateKey);
+        return false;
+    }
+
+    private static List<(string gateway, string node)> GenerateCandidateCuts(TGraph graph)
+    {
+        var candidates = new List<(string gateway, string node)>();
         foreach (var gateway in graph.Gateways.OrderBy(g => g, StringComparer.Ordinal))
-            foreach (var neighbour in graph.GetSortedNeighbours(gateway))
-                gatewayEdges.Add($"{gateway}-{neighbour}");
-        return $"{virusPosition}|{string.Join(",", gatewayEdges)}";
+            foreach (var neighbour in graph.GetNeighbours(gateway, sort: NeighboursSort.ByAscending))
+                candidates.Add((gateway, neighbour));
+        
+        return candidates;
+    }
+
+    private static bool TrySimulateVirusMove(TGraph graph, string currentPosition, [NotNullWhen(true)] out string? nextPosition)
+    {
+        nextPosition = null;
+        var distancesAfterCut = graph.FindShortestPathsDistances(currentPosition);
+        if (!TryFindVirusBestTarget(graph, distancesAfterCut, out var newTargetGateway))
+            return false;
+
+        var distancesToNewTarget = graph.FindShortestPathsDistances(newTargetGateway);
+        return TryFindNextStepTowardsTarget(graph, currentPosition, distancesToNewTarget, out nextPosition);
+    }
+    
+    private static string CreateStateKey(TGraph graph, string virusPosition)
+    {
+        var sb = new StringBuilder(virusPosition);
+        sb.Append('|');
+        var gatewayEdges = graph.Gateways
+            .OrderBy(g => g, StringComparer.Ordinal)
+            .SelectMany(g => graph.GetNeighbours(g).Select(n => $"{g}-{n}"));
+        sb.Append(string.Join(",", gatewayEdges));
+        return sb.ToString();
     }
 
     private static bool TryFindVirusBestTarget(
-        TGraph graph, PathsInfo pathsInfo, [NotNullWhen(true)] out string? bestTarget)
+        TGraph graph, IReadOnlyDictionary<string, int> distances, [NotNullWhen(true)] out string? bestTarget)
     {
         bestTarget = null;
         var minDistance = int.MaxValue;
 
         foreach (var gateway in graph.Gateways.OrderBy(g => g, StringComparer.Ordinal))
         {
-            if (pathsInfo.Distances.TryGetValue(gateway, out var distance) && distance < minDistance)
+            if (distances.TryGetValue(gateway, out var distance) && distance < minDistance)
             {
                 minDistance = distance;
                 bestTarget = gateway;
@@ -192,15 +251,15 @@ public class VirusSolver<TGraph> where TGraph : IGraph
     }
     
     private static bool TryFindNextStepTowardsTarget(
-        TGraph graph, string currentNode, PathsInfo distancesToTarget, [NotNullWhen(true)] out string? nextStep)
+        TGraph graph, string currentNode, IReadOnlyDictionary<string, int> distancesToTarget, [NotNullWhen(true)] out string? nextStep)
     {
         nextStep = null;
-        if (!distancesToTarget.Distances.TryGetValue(currentNode, out var currentDistance))
+        if (!distancesToTarget.TryGetValue(currentNode, out var currentDistance))
             return false;
         
-        foreach (var neighbour in graph.GetSortedNeighbours(currentNode))
+        foreach (var neighbour in graph.GetNeighbours(currentNode, sort: NeighboursSort.ByAscending))
         {
-            if (distancesToTarget.Distances.TryGetValue(neighbour, out var neighbourDistance) && 
+            if (distancesToTarget.TryGetValue(neighbour, out var neighbourDistance) && 
                 neighbourDistance < currentDistance)
             {
                 nextStep = neighbour;
@@ -215,7 +274,7 @@ public class Program
 {
     public static void Main()
     {
-        var edges = new List<(string, string)>();
+        var edges = new HashSet<(string, string)>();
         while (Console.ReadLine()?.Trim() is { } line && !string.IsNullOrEmpty(line))
         {
             var parts = line.Split('-');
@@ -223,7 +282,7 @@ public class Program
                 edges.Add((parts[0], parts[1]));
         }
 
-        var graph = new Graph(edges);
+        var graph = (Graph)Graph.FromEdges(edges);
         var solver = new VirusSolver<Graph>();
         var result = solver.Solve(graph, "a");
         
